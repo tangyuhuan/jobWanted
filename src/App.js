8688781b0b4b24b775535b977215325bb5745148
雨欢 <yuhuan@yuhuandeMacBook-Pro.local>
2018-07-22 22:41:05 +0800

    使用react-redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 import { Button } from 'antd-mobile';
 import 'antd-mobile/dist/antd-mobile.css'; 
+import { connect } from 'react-redux'
+import {addGUN,removeGUN,addGunAsync} from './index.redux'
 class App extends Component {
-  // constructor(props){
-  //   super(props);
-  // }
+  //store.dispatch的已经不需要了 addGUN等已经自动有dispatch功能了
   render() {
-    const store  = this.props.store;
-    const num = store.getState();
-    const{addGUN,removeGUN,addGunAsync}=this.props;
+    const{num, addGUN, removeGUN, addGunAsync}=this.props;
     return (
-      <div className="App">
+      <div>
         <h1>number of guns:{num}</h1>
-        <Button type="primary" onClick={()=>store.dispatch(addGUN())}>申请武器</Button>
-        <Button type="primary" onClick={()=>store.dispatch(removeGUN())}>上交武器</Button>
-        <Button type="primary" onClick={()=>store.dispatch(addGunAsync())}>拖两天再给</Button>
+        <Button type="primary" onClick={addGUN}>申请武器</Button>
+        <Button type="primary" onClick={removeGUN}>上交武器</Button>
+        <Button type="primary" onClick={addGunAsync}>拖两天再给</Button>
       </div>
     );
   }
 }
-
+const mapStatetoProps=(state)=>{
+  return { num:state }
+}
+const actionCreators = {addGUN,removeGUN,addGunAsync} 
+App = connect(mapStatetoProps,actionCreators)(App)
+//属性和方法分别给到组件App，这样react-redux会自动把这些参数放到App的props里面
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore , applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
 import App from './App';
-import { counter,addGUN,removeGUN,addGunAsync} from './index.redux'
+import { counter } from './index.redux'
 
 //新建store，以props形式传给App
 const store = createStore(counter,compose(
@@ -11,9 +12,9 @@ const store = createStore(counter,compose(
 	window.devToolsExtension? window.devToolsExtension():()=>{}
 ))
 // ReactDOM.render(<App store={store} addGUN={addGUN} removeGUN={removeGUN}/>, document.getElementById('root'));
-function render(){
-	ReactDOM.render(<App store={store} addGUN={addGUN} removeGUN={removeGUN} addGunAsync={addGunAsync}/>, document.getElementById('root'));
-}
-
-render()
-store.subscribe(render) //订阅render函数，每次render有状态变化都会重新执行
+ReactDOM.render(
+	(<Provider store={store}>
+		<App/>
+	</Provider>),
+	document.getElementById('root')
+);
