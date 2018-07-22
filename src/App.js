362cc48b17bc3ed56be649a0c812c3a2e63dfb27
雨欢 <yuhuan@yuhuandeMacBook-Pro.local>
2018-07-22 14:45:07 +0800

    remove unused packages and add redux

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Button type="primary">primary</Button>
+        <Button type="primary">primary333</Button>
       </div>
     );
   }
