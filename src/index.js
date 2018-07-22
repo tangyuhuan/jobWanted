99308f5d2fed0da6da5368d5debdc1826dc1bfde
雨欢 <yuhuan@yuhuandeMacBook-Pro.local>
2018-07-22 21:56:25 +0800

    使用redux扩展程序

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore , applyMiddleware } from 'redux'
+import { createStore , applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import App from './App';
 import { counter,addGUN,removeGUN,addGunAsync} from './index.redux'
+
 //新建store，以props形式传给App
-const store = createStore(counter,applyMiddleware(thunk))
+const store = createStore(counter,compose(
+	applyMiddleware(thunk),
+	window.devToolsExtension? window.devToolsExtension():()=>{}
+))
 // ReactDOM.render(<App store={store} addGUN={addGUN} removeGUN={removeGUN}/>, document.getElementById('root'));
 function render(){
 	ReactDOM.render(<App store={store} addGUN={addGUN} removeGUN={removeGUN} addGunAsync={addGunAsync}/>, document.getElementById('root'));
